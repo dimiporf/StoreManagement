0c8c924a2818419cfe993043c580ad1cdcfded6c
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-07-13 12:01:48 +0200

    Rolling back to previous migration

---
+++ StoreBackend/Migrations/202407130904217_AddInventoryStock.cs
@@ -0,0 +1,37 @@
+﻿namespace StoreBackend.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class AddInventoryStock : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.InventoryStocks",
+                c => new
+                {
+                    InventoryStockID = c.Int(nullable: false, identity: true),
+                    InventoryItemID = c.Int(nullable: false),
+                    WarehouseID = c.Int(nullable: false),
+                    Quantity = c.Int(nullable: false),
+                    MovingAverageCost = c.Decimal(nullable: false, precision: 18, scale: 2),
+                })
+                .PrimaryKey(t => t.InventoryStockID)
+                .ForeignKey("dbo.InventoryItems", t => t.InventoryItemID, cascadeDelete: true)
+                .ForeignKey("dbo.Warehouses", t => t.WarehouseID, cascadeDelete: true)
+                .Index(t => t.InventoryItemID)
+                .Index(t => t.WarehouseID);
+        }
+
+        public override void Down()
+        {
+            DropForeignKey("dbo.InventoryStocks", "WarehouseID", "dbo.Warehouses");
+            DropForeignKey("dbo.InventoryStocks", "InventoryItemID", "dbo.InventoryItems");
+            DropIndex("dbo.InventoryStocks", new[] { "WarehouseID" });
+            DropIndex("dbo.InventoryStocks", new[] { "InventoryItemID" });
+            DropTable("dbo.InventoryStocks");
+        }
+    }
+
+}
---
+++ StoreBackend/Models/InventoryStock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreBackend.Models
+{
+    public class InventoryStock
+    {
+        [Key]
+        public int InventoryStockID { get; set; }
+
+        // Foreign key referencing the InventoryItem entity
+        public int InventoryItemID { get; set; }
+
+        // Foreign key referencing the Warehouse entity
+        public int WarehouseID { get; set; }
+
+        // Quantity of items in stock
+        public int Quantity { get; set; }
+
+        // Moving average cost per item
+        public decimal MovingAverageCost { get; set; }
+
+        // Navigation property to represent the relationship with InventoryItem
+        public virtual InventoryItem InventoryItem { get; set; }
+
+        // Navigation property to represent the relationship with Warehouse
+        public virtual Warehouse Warehouse { get; set; }
+    }
+}
